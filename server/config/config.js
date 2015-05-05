d1169f8e0ac30793305a21371bf4074cc85a35bc
nikhil <nikhil@cronj.com>
2015-05-06 00:09:14 +0530

    show parent and immediate child

--- server/config/config.js
+++ server/config/config.js
@@ -11,21 +11,21 @@ module.exports = {
         username: '',
         password: ''
     },
-    map: {
-        header1 : 'Tenants',
-        header2 : 'Global',
-        type1   : 'Export-files',
-        type2   : 'Import-files',
-        type3   : 'Product-files',
-        type4   : 'Export-formats',
-        type5   : 'Import-formats',
-        type6   : 'logs'
-    }   
-
     // map: {
-    //     header1 : 'FolderA',
-    //     header2 : 'FolderB',
-    //     type1   : 'FolderC',
-    //     type2   : 'FolderD'
-    // } 
+    //     header1 : 'Tenants',
+    //     header2 : 'Global',
+    //     type1   : 'Export-files',
+    //     type2   : 'Import-files',
+    //     type3   : 'Product-files',
+    //     type4   : 'Export-formats',
+    //     type5   : 'Import-formats',
+    //     type6   : 'logs'
+    // }   
+
+    map: {
+        header1 : 'FolderA',
+        header2 : 'FolderB',
+        type1   : 'FolderC',
+        type2   : 'FolderD'
+    } 
 };
--- server/controller/user.js
+++ server/controller/user.js
@@ -45,53 +45,39 @@ exports.getTenantUrl = {
         }  
       }
       file_path = '../server/Files' + path;
-      dirTree(file_path, true);
+      console.log("file_path",file_path);
+      var a = dirTree(file_path, true);
+      console.log("a",a);
+       return reply(a);
     }  
   }
 };
-
 
 
 function dirTree(filename, flag) {
-  var stats = fs.lstatSync(filename);
-  info = {
-      path: '/' + Path.basename(filename),
-      name: Path.basename(filename)
-  };
-  if(stats.isDirectory()) {
-      info.type = "folder";
-      if(flag)
-      {  
-        info.children = fs.readdirSync(filename).map(function(child) {
+    var stats = fs.lstatSync(filename),
+        info = {
+            path: filename,
+            name: Path.basename(filename)
+        };
+
+    if (stats.isDirectory()) {
+        console.log("info",info);
+        info.type = "folder";
+        if(flag)
+        {
+          info.children = fs.readdirSync(filename).map(function(child) {
             return dirTree(filename + '/' + child, false);
-        });
-      }  
-  } 
-  else 
-  {
-    info.type = "file";
-  }
-  return info;
-};
+          });
+        }  
+    } 
+    else 
+    {
+        info.type = "file";
+    }
 
-function childTree(filename)
-{
-  console.log("fahsdfdghsdh");
-  var stats = fs.lstatSync(filename),
-  info = {
-      path: '/' + Path.basename(filename),
-      name: Path.basename(filename)
-  };
-  if(stats.isDirectory()) {
-      info.type = "folder";
-  } 
-  else 
-  {
-    info.type = "file";
-  }
-  return info;
-};
-
+    return info;
+}
 
 exports.getGlobalUrl = {
   handler: function(request, reply) {
@@ -107,105 +93,7 @@ exports.getGlobalUrl = {
           path += '/'+ request.params.file_name;
         }  
       }
-      return reply(path);
+      return reply(path)
     }  
   }
 };
-/*
-
-exports.getTenantUrl = {
-  handler:function(request, reply) {
-    var path = "";
-    if(request.params.header)
-    {
-      path = '/' + Config.map[request.params.header]; 
-      if(request.params.id)
-      {
-        path += '/' + request.params.id;
-        if(request.params.type)
-        {
-          path += '/' + Config.map[request.params.type]
-          if(request.params.file_name)
-          {
-            path += '/'+ request.params.file_name;
-          }  
-        }
-        var file_path = '../server/Files' + path;  
-      }
-      //var a = dirtree(file_path);
-      console.log("a",file_path);
-    }  
-  }
-};
-
-exports.getGlobalUrl = {
-  handler: function(request, reply) {
-    var path = "";
-    if(request.params.header2)
-    {
-      path = '/' + Config.map[request.params.header2];
-      if(request.params.type)
-      {
-        path += '/' + Config.map[request.params.type]
-        if(request.params.file_name)
-        {
-          path += '/'+ request.params.file_name;
-        }  
-      }
-      return reply(path);
-    }  
-  }
-};
-
-function dirtree(file_path){
-
-  fs.readdir(file_path, function (err, files) {
-  if (err) {
-      throw err;
-  }
-  else
-  {
-    var info = {
-      path: '/' + Path.basename(file_path),
-      name: Path.basename(file_path)
-    }; 
-    if(path.extname(info.name))
-    {
-      info.type = "file";
-    }
-    else
-    {
-      info.type = "folder";
-    }
-    console.log("info",info);
-    files.forEach(function (file) {
-      console.log("xx",Path.extname(file));
-    })    
-  }  
-})
-};
-
-// function dirTree(filename) {
-//   var stats = fs.lstatSync(filename),
-//   info = {
-//       path: '/' + Path.basename(filename),
-//       name: Path.basename(filename)
-//   };
-//   if(stats.isDirectory()) {
-//       info.type = "folder";
-//       info.children = fs.readdirSync(filename).map(function(child) {
-//           //structure(child, info);
-//           child.path = '/'+ info.name + '/' + child;
-//           child.name = child;
-//           return child;
-//           //return('/'+ info.name + '/' + child);
-//       });
-//   } 
-//   else 
-//   {
-//     info.type = "file";
-//   }
-//   return info;
-// };
-
-*/
