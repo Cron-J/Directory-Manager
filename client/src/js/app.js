0b5148728f908456faf2899ffe9e64c5444b6ddb
nikhil <nikhil@cronj.com>
2015-05-09 01:31:27 +0530

    add path in the address bar

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -5,58 +5,46 @@ app.config(function($routeProvider){
 	$routeProvider
     	.when('/', { 
         	controller:'DirectoryManagerController', 
-        	templateUrl:'views/a.html'
+        	templateUrl:'views/directory.html'
       	})
 
       	.when('/:param*',{
       		controller:'DirectoryManagerController',
-      		templateUrl:'views/b.html'
+      		templateUrl:'views/directory.html'
       	})
       
       	.otherwise('/')
 });
 				
-app.controller('DirectoryManagerController', ['$scope', '$http', function ($scope, $http) {
+app.controller('DirectoryManagerController', ['$scope', '$http', '$location', function ($scope, $http, $location) {
 
-	$scope.init_stage = function ()
+	function init_stage()
 	{
 		$http.get('/searchDirectory/')
 		.success(function(data, status) {
-			$scope.data = [];
-			for(i=0;i<data.children.length;i++)
-			{
-				$scope.data[i] = {name:"",path:"",type:"",download_path:""};
-				$scope.data[i].name = data.children[i].name;
-				$scope.data[i].path = data.children[i].user_path;
-				$scope.data[i].type = data.children[i].type;
-				if(data.children[i].path)
-				{
-					var split = data.children[i].path.split('../client/src/');
-					$scope.data[i].download_path = split[1];
-				}	
-			}
+			$scope.data = sortArray(data);
 			$scope.arr = [];
 		});	
 	};
 
-	$scope.init_stage();
+	function init()
+	{
+		var param = window.location.href.split('/#/');
+		if(param[1])
+		{
+			getdata(param[1]);
+		}
+		else
+		{
+			init_stage();
+		}	
+	};
+	init();
 
 	function getdata(query) {
-		$http.get('/searchDirectory' + query)
+		$http.get('/searchDirectory/' + query)
 		.success(function(data, status) {
-			$scope.data = [];
-			for(i=0;i<data.children.length;i++)
-			{
-				$scope.data[i] = {name:"",path:"",type:"",download_path:""};
-				$scope.data[i].name = data.children[i].name;
-				$scope.data[i].path = data.children[i].user_path;
-				$scope.data[i].type = data.children[i].type;
-				if(data.children[i].path)
-				{
-					var split = data.children[i].path.split('../client/src/');
-					$scope.data[i].download_path = split[1];
-				}
-			}
+			$scope.data = sortArray(data);
 			$scope.data.root = data.user_path;
 			var split = $scope.data.root.split("/");
 			$scope.arr = [];
@@ -64,25 +52,44 @@ app.controller('DirectoryManagerController', ['$scope', '$http', function ($scop
 				$scope.arr.push(split[i]);
 			}
 		})
-	}
+	};
+
+	function sortArray(data)
+	{
+		var result = [];
+		for(i=0;i<data.children.length;i++)
+		{
+			result[i] = {name:"",path:"",type:"",download_path:""};
+			result[i].name = data.children[i].name;
+			result[i].path = data.children[i].user_path;
+			result[i].type = data.children[i].type;
+			if(data.children[i].path)
+			{
+				var split = data.children[i].path.split('../client/src/');
+				result[i].download_path = split[1];
+			}
+		}
+		return result;
+	};
 
 	$scope.show_children = function(path)
 	{
-		getdata(path);
+		var trimPath = path.substring(path.indexOf('/')+1);
+		$location.path('/' + trimPath);
 	};
 
 	$scope.back_children = function(data, val)
 	{
 		if(val === 'Root')
 		{
-			$scope.init_stage();
-			$scope.arr = [];
+			init_stage();
+			$location.path('/');
 		}
 		else
 		{
-			var split = data.root.split("/" + val);
-			var query = split[0] + "/" + val;
-			getdata(query);			
+			var split = data.root.split(val);
+			var trimPath = split[0] + val;
+			$location.path(trimPath);		
 		}	
 	}
 	
